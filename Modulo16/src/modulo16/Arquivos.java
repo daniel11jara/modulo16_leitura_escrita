65de0972f73547395492ebc95f559dc7133ce6d1
daniel jara <daniel11jara@gmail.com>
2023-03-30 22:40:15 -0300

    modulo 16

--- Modulo16/src/modulo16/Arquivos.java
+++ Modulo16/src/modulo16/Arquivos.java
@@ -4,11 +4,33 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.List;
 
 public class Arquivos {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		
+		Pessoa pessoa1 = new Pessoa();
+		pessoa1.setEmail("pessoa1@gmail.com");
+		pessoa1.setIdade(50);
+		pessoa1.setNome("rubens");
+		
+		Pessoa pessoa2 = new Pessoa();
+		pessoa2.setEmail("pessoa2@gmial.com");
+		pessoa2.setIdade(56);
+		pessoa2.setNome("carlos");
+		
+		Pessoa pessoa3 = new Pessoa();
+		pessoa3.setEmail("pessoa3@gmail.com");
+		pessoa3.setIdade(28);
+		pessoa3.setNome("Jessica");
+		
+		List<Pessoa> pessoas = new ArrayList<Pessoa>();
+		pessoas.add(pessoa1);
+		pessoas.add(pessoa2);
+		pessoas.add(pessoa3);
+				
 		
 		//classe que cria arquivo
 		File arquivo = new File("C:\\Users\\danie\\git\\modulo16_leitura_escritab\\Modulo16\\src\\modulo16\\arquivo.tx");
@@ -19,6 +41,11 @@ public class Arquivos {
 		
 		//classe que escreve no arquivo
 		FileWriter escrever_no_arquivo = new FileWriter(arquivo);
+				
+		for (Pessoa p : pessoas) {
+			escrever_no_arquivo.write(p.getNome() + ";" + p.getEmail() + p.getIdade());
+		} 
+		
 		
 		for (int i = 1; i<=10; i++) {
 			escrever_no_arquivo.write("Texto da minha linha " + i + "\n");
